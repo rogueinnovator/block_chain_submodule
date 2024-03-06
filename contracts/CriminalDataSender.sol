6b7b9975126161302dea7b998cb3b9f062dea1c5
rogueinnovator <sadaat185336@gmail.com>
2024-03-06 21:31:23 +0500

    block-chain

--- contracts/CriminalDataSender.sol
+++ contracts/CriminalDataSender.sol
@@ -2,10 +2,17 @@
 pragma solidity ^0.8.8;
 contract CriminalDataSender{
     uint public DataCount = 10;
-    struct Criminal_Entity{
+    struct CriminalEntity{
         uint id;
         string name;
- uint cnic;
-    
+        uint cnic;
     }
+    mapping(uint => CriminalEntity) public criminalsEnttities;
+    constructor(){
+            ("criminals data inserted");
+    }
+    function CreateEntity(string memory _name,uint _cnic) public{
+        
+    }
+
 }
